2b03ee4a4a88d654d0ca61a8b23704dfcac90b45
shubha agrawal <shubh.cap@gmail.com>
2023-02-15 10:00:35 -0500

    Easy Going answered

--- index.js
+++ index.js
@@ -403,15 +403,102 @@ Answer: 50% time should be spent in thinking vs actually typing a code as thinki
 
 // V. Objects
 // A.
+
+    const user = {
+        name: "Shubha",
+        email: "shubh.cap@gmail.com",
+        age: 38,
+        purchased: []
+    }
 // B.
+
+    user.email = "abcd@gmail.com";
+    user.age++;
+    console.log(user);
 // C.
+
+    user.location = "pittsburgh";
+
 // D.
+
+    user.purchased.push("carbohydrates");
+    user.purchased.push("peace of mind");
+    user.purchased.push("Merino jodhpurs");
+
+    console.log(user.purchased[2]);
 // E.
+
+    user.friend = {
+    name: "Grace Hopper",
+    age: 85,
+    location: "California",
+    purchased: []
+    }
+
+    console.log(user.friend.name);
+    console.log(user.friend.location);
+    user.friend.age = 55;
+    user.friend.purchased.push("The One Ring");
+    user.friend.purchased.push("A latte");
+    console.log(user.friend.purchased[1]);
+
 // F.
+    console.log("---------User purchased Array ---------");
+    for(let i=0; i<user.purchased.length;i++){
+        console.log(user.purchased[i]);
+    }
+
+    console.log("---------Friend's purchased Array ---------");
+    for(let i=0; i<user.friend.purchased.length;i++){
+        console.log(user.friend.purchased[i]);
+    }
+    
 // G.
+
+    function updateUser(){
+        user.age++;
+        user.name = user.name.toUpperCase();
+        console.log(user);
+    }
+
+    function oldAndLoud(person){
+        person.age++;
+        person.name = person.name.toUpperCase();
+        console.log(person);
+    }
+
+    // updateUser();
+    oldAndLoud(user);
 //_____________________________________________________________
 
 
 // Extra
+//Cat Combinator
 
+const cat1 = {
+    name: "Fussy",
+    breed: "BullDog",
+    age: 10
+}
 
+console.log(cat1.age);
+console.log(cat1.breed);
+
+const cat2 = {
+    name: "Boggy",
+    breed: "Poodle",
+    age: 5
+}
+
+function combineCats(mama,papa){
+    const mamapapa = {
+        name: mama.name+papa.name,
+        age:1,
+        breed:mama.breed+"-"+papa.breed
+    }
+    return mamapapa;
+}
+
+console.log(combineCats(cat1,cat2));
+console.log(combineCats(combineCats(cat1,cat2),combineCats(cat1,cat2)));
+console.log(combineCats(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)),combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2))));
